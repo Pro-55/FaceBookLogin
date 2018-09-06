925b26950fbe80f07af10155b1493e73038fc80a
Sahil Pawar <pawarsahil94.sp@gmail.com>
2018-09-06 18:51:01 +0530

    Login PopUp Implemented.

--- app/src/main/java/com/example/admin/testlogin/SettingsActivity.java
+++ app/src/main/java/com/example/admin/testlogin/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin.testlogin;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -11,6 +15,9 @@ public class SettingsActivity extends AppCompatActivity {
 
     private TextView versionText;
     private Switch switchOne, switchTwo;
+    private Button buttonLogin, buttonLoginFB, buttonLoginTwitter, buttonLoginGoogle;
+    private AlertDialog.Builder mBuilder;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,14 @@ public class SettingsActivity extends AppCompatActivity {
 
         versionText = findViewById(R.id.versionText);
         versionText.setText("Version: " + BuildConfig.VERSION_NAME);
+
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginDialog();
+            }
+        });
 
         switchOne = findViewById(R.id.switchOne);
         switchOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -44,5 +59,43 @@ public class SettingsActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    private void goToLogin() {
+        Intent loginIntent = new Intent(SettingsActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void openLoginDialog() {
+        mBuilder = new AlertDialog.Builder(SettingsActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.login_dialog, null);
+
+        buttonLoginGoogle = view.findViewById(R.id.buttonLoginGoogle);
+        buttonLoginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+        buttonLoginFB = view.findViewById(R.id.buttonLoginFB);
+        buttonLoginFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+        buttonLoginTwitter = view.findViewById(R.id.buttonLoginTwitter);
+        buttonLoginTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+        mBuilder.setView(view);
+        alertDialog = mBuilder.create();
+        alertDialog.show();
     }
 }
